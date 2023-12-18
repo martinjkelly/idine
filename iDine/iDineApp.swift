40bc15752bba3ffc0147bb97bcefe1f589a2c284
Martin J Kelly <martin.j.kelly@aexp.com>
2023-12-18 10:45:52 +0000

    created a basic menu - list

--- iDine/iDineApp.swift
+++ iDine/iDineApp.swift
@@ -1,17 +1,10 @@
-//
-//  iDineApp.swift
-//  iDine
-//
-//  Created by Martin James Kelly on 18/12/2023.
-//
-
 import SwiftUI
 
 @main
 struct iDineApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
