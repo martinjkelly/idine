1c0f3e65bf874b08ab35e4333e03d3b48960b706
Martin J Kelly <martin.j.kelly@aexp.com>
2023-12-18 16:15:34 +0000

    finishing touches

--- iDine/Views/OrderView.swift
+++ iDine/Views/OrderView.swift
@@ -14,16 +14,25 @@ struct OrderView: View {
               Text("$\(item.price)")
             }
           }
+          .onDelete(perform: deleteItems)
         }
         
         Section {
           NavigationLink("Place Order") {
             CheckoutView()
           }
+          .disabled(order.items.isEmpty)
         }
       }
       .navigationTitle("Order")
+      .toolbar() {
+        EditButton()
+      }
     }
+  }
+  
+  func deleteItems(at offsets: IndexSet) {
+    order.items.remove(atOffsets: offsets)
   }
 }
 
