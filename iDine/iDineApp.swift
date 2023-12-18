b28648efc8e72f3fcf04a50a3aefb830ce99d5d3
Martin J Kelly <martin.j.kelly@aexp.com>
2023-12-18 10:31:39 +0000

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Martin James Kelly on 18/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
